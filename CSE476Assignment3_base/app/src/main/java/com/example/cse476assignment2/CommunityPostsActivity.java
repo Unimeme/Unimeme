b350d3a3da18cfacf777b6616cfce4f651c543fe
Varun Verma <vermava1@msu.edu>
2025-11-06 16:28:30 -0500

    Fixed a minor error
    Quickly creating a working base case for me to start my work

--- CSE476Assignment3_base/app/src/main/java/com/example/cse476assignment2/CommunityPostsActivity.java
+++ CSE476Assignment3_base/app/src/main/java/com/example/cse476assignment2/CommunityPostsActivity.java
@@ -12,6 +12,7 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
